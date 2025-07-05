afbe9cd5b864290646d1132a8caa2a40342a6f98
softworkz <softworkz@hotmail.com>
2025-07-06 01:25:28 +0200

    Move all URLs to Globals class

---
+++ Vault/Explorer/Globals.cs
@@ -0,0 +1,15 @@
+﻿namespace Microsoft.Vault.Explorer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal static class Globals
+    {
+        public const string GitHubUrl = "https://github.com/reysic/AzureKeyVaultExplorer";
+        public const string GitHubIssuesUrl = "https://github.com/reysic/AzureKeyVaultExplorer/issues";
+        public const string ActivationUrl = "https://srnk.cc/ve";
+    }
+}
