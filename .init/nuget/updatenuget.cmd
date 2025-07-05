d4913edea1944be77e3b1d913d4bb0b53d684584
softworkz <softworkz@hotmail.com>
2025-07-05 23:09:47 +0200

    Remove obsolete stuff

--- .init/nuget/updatenuget.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REM Copyright (c) Microsoft Corporation. All rights reserved. 
-REM Licensed under the MIT License. See License.txt in the project root for license information. 
-
-powershell -ExecutionPolicy Unrestricted -Command "iex \"^& '%~dp0\updatenuget.ps1' '%~dp0\nuget.exe'\""
--- .init/nuget/updatenuget.ps1
+++
@@ -1,4 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved. 
-# Licensed under the MIT License. See License.txt in the project root for license information. 
-
-Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile $args[0]
