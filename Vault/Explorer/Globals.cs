f65faaa292d9856347839b2e3c6746584e32c758
softworkz <softworkz@hotmail.com>
2025-07-06 05:44:07 +0200

    Fix activation

--- Vault/Explorer/Globals.cs
+++ Vault/Explorer/Globals.cs
@@ -8,6 +8,7 @@
 
     internal static class Globals
     {
+        public const string OnlineActivationUri = "https://softworkz.github.io/AzureKeyVaultExplorer/VaultExplorer.application";
         public const string GitHubUrl = "https://github.com/softworkz/AzureKeyVaultExplorer";
         public const string GitHubIssuesUrl = "https://github.com/softworkz/AzureKeyVaultExplorer/issues";
         public const string ActivationUrl = "https://srnk.cc/ve";
