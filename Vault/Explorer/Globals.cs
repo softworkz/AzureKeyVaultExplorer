0fbe7e2cae51f0557fabeee6f0d3d877a73d28fe
softworkz <softworkz@hotmail.com>
2025-07-14 11:06:43 +0200

    Update link URL

--- Vault/Explorer/Globals.cs
+++ Vault/Explorer/Globals.cs
@@ -5,6 +5,6 @@ namespace Microsoft.Vault.Explorer
         public const string OnlineActivationUri = "https://softworkz.github.io/AzureKeyVaultExplorer/VaultExplorer.application";
         public const string GitHubUrl = "https://github.com/softworkz/AzureKeyVaultExplorer";
         public const string GitHubIssuesUrl = "https://github.com/softworkz/AzureKeyVaultExplorer/issues";
-        public const string ActivationUrl = "https://srnk.cc/ve";
+        public const string ActivationUrl = "https://emby.media/ve";
     }
 }
